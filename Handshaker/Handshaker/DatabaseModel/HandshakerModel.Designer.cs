d3f4c47d7eb3a1d121fb203168dacb8f89d7fcd3
Yunus Emre ÇAKMAZ <ynsemre158@gmail.com>
2016-02-18 20:00:16 +0200

    User Tablosuna gerekli attribute'ler eklendi.

--- Handshaker/Handshaker/DatabaseModel/HandshakerModel.Designer.cs
+++ Handshaker/Handshaker/DatabaseModel/HandshakerModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\HandShaker\Handshaker\Handshaker\DatabaseModel\HandshakerModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Yunus Emre\Documents\Handshaker-MW\Data\Handshaker\Handshaker\DatabaseModel\HandshakerModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
--- Handshaker/Handshaker/DatabaseModel/HandshakerModel.edmx.sql
+++ Handshaker/Handshaker/DatabaseModel/HandshakerModel.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
 -- --------------------------------------------------
--- Date Created: 02/14/2016 00:05:01
--- Generated from EDMX file: C:\HandShaker\Handshaker\Handshaker\DatabaseModel\HandshakerModel.edmx
+-- Date Created: 02/18/2016 19:57:15
+-- Generated from EDMX file: C:\Users\Yunus Emre\Documents\Handshaker-MW\Data\Handshaker\Handshaker\DatabaseModel\HandshakerModel.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -22,6 +22,9 @@ GO
 -- Dropping existing tables
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[UserSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UserSet];
+GO
 
 -- --------------------------------------------------
 -- Creating all tables
@@ -29,7 +32,12 @@ GO
 
 -- Creating table 'UserSet'
 CREATE TABLE [dbo].[UserSet] (
-    [Id] int IDENTITY(1,1) NOT NULL
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL,
+    [Username] nvarchar(max)  NOT NULL,
+    [Email] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL
 );
 GO
 
