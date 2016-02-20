139cdb58da1267f2384f8e2b0092c5fad0cba0e4
Yunus Emre ÇAKMAZ <ynsemre158@gmail.com>
2016-02-20 19:11:00 +0200

    SignUp ve SignIn UseCase'leri genel hatlarıyla geliştirildi.

--- Handshaker/Handshaker/Codes/Providers/UserProvider.cs
+++ Handshaker/Handshaker/Codes/Providers/UserProvider.cs
@@ -11,18 +11,23 @@ namespace Handshaker.Codes.Providers
     public class UserProvider
     {
         /// <summary>
-        /// Add user to UserSet
+        /// Register new user
         /// </summary>
-        /// <returns></returns>
-        public int InsertUser(UserModel userModel)
+        /// <returns>new user Id</returns>
+        public int SignUp(UserModel userModel)
         {
             int userId = 0;
 
             using (HandshakerEntities handshakerEntityContainer = new HandshakerEntities())
             {
+                if (handshakerEntityContainer.UserSet.Count(u => u.Username.Equals(userModel.Username)) > 0)
+                    throw new Exception("Sistemde " + userModel.Username + " adıyla bir kullanıcı bulunmaktadır");
+
+                #region hash password
                 byte[] passwordAsByte = System.Text.Encoding.ASCII.GetBytes(userModel.Password);
                 passwordAsByte = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAsByte);
                 String hashedPassword = System.Text.Encoding.ASCII.GetString(passwordAsByte);
+                #endregion
 
                 User newUser = new User()
                 {
@@ -40,6 +45,25 @@ namespace Handshaker.Codes.Providers
             }
 
             return userId;
+        }
+
+        public void SignIn(string userNameOrMail,string password)
+        {
+            #region hash password
+                byte[] passwordAsByte = System.Text.Encoding.ASCII.GetBytes(password);
+                passwordAsByte = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAsByte);
+                String hashedPassword = System.Text.Encoding.ASCII.GetString(passwordAsByte);
+            #endregion
+
+            using (HandshakerEntities handshakerEntityContainer = new HandshakerEntities())
+            {
+                User user = handshakerEntityContainer.UserSet.FirstOrDefault(u => (u.Username.Equals(userNameOrMail) || u.Email.Equals(userNameOrMail)) && u.Password.Equals(hashedPassword));
+
+                if(user==null)
+                    Console.WriteLine("Hatalı kullanıcı adı ya da parola");
+                else
+                    Console.WriteLine("Handshaker uygulamamıza hoşgeldiniz");
+            }
         }
     }
 }
